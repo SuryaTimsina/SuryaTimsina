8615573d171560d68e5f40bef4c551184487b4cf
Surya Timsina <surya.timsina@lethbridgecollege.ca>
2024-01-22 15:43:12 -0700

    new entry

--- app/index.js
+++ app/index.js
@@ -6,7 +6,8 @@ import Styles from '../styles/page-styles';
 
 
 export default function page() {
-const [food, onChangeFood] = React.useState("");
+    const [food, onChangeFood] = React.useState("");
+    const [userAge, onChangeAge] = React.useState("");
     return (
         <View style={Styles.page} >
             <Text> This is the Index Page </Text>
@@ -16,10 +17,20 @@ const [food, onChangeFood] = React.useState("");
                 value={food}
                 placeholder="choose your favourite food" />
 
+            <TextInput
+                style={Styles.input}
+                onChangeText={onChangeAge}
+                int userAge
+                value={userAge}
+                placeholder="What is your age" />
+           
+
+
+
             <Link style={Styles.button}
                 href={{
                 pathname: "/page2",
-                params: {food}
+                params: {food,userAge},
                 }} asChild >
                 <Pressable>
                     <Text>  Page 2 </Text>
--- app/page2.js
+++ app/page2.js
@@ -6,11 +6,13 @@ import Styles from '../styles/page-styles';
 
 export default function Page() {
     const params = useLocalSearchParams();
-    const { food } = params;
+    const { food, userAge} = params;
     return (
         <View style={Styles.page}>
             <Text> This is the 2nd Page </Text>
-            <Text> You like to eat{food }!</Text>
+            <Text> You like to eat{food}!</Text>
+            <Text> Your Age is:{userAge}!</Text>
+            
             <Link href="/">Home</Link>
         </View>
 
